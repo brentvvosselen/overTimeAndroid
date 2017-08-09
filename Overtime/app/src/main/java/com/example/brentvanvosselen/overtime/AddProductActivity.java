6ec5819e69a7fcf30e4d42b451109601af6afd90
Brent Van Vosselen <brentvanvosselen@Brents-MacBook-Pro.local>
2017-08-09 23:35:04 +0200

    delete actionbar

--- Overtime/app/src/main/java/com/example/brentvanvosselen/overtime/AddProductActivity.java
+++ Overtime/app/src/main/java/com/example/brentvanvosselen/overtime/AddProductActivity.java
@@ -3,12 +3,15 @@ package com.example.brentvanvosselen.overtime;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
 
 public class AddProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_add_product);
 
 
